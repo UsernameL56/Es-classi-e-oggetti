88cc3bd289517e7ac14e4e83ab0db0c05e69cda6
corsi.20188 <corsi.20188@E2-9-PC26>
2023-09-19 12:37:50 +0200

    Aggiungere i file di progetto.

---
+++ Es classi e oggetti/Classe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_classi_e_oggetti
+{
+    internal class Classe
+    {
+        public class Prodotto
+        {
+            private string nome;
+            private double prezzo;
+
+            public Prodotto() { }
+
+            public Prodotto(string nomeVal, double prezzoVal) 
+            { 
+                nome = nomeVal;
+                prezzo= prezzoVal;
+            }
+        }
+    }
+}
---
+++ Es classi e oggetti/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Classe;
+namespace Es_classi_e_oggetti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
