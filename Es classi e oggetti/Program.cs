2198b488ce70000a1da19fdf7761d06bb227d51e
corsi.20188 <corsi.20188@E2-9-PC26>
2023-09-19 13:22:18 +0200

    Fine programma

--- Es classi e oggetti/Program.cs
+++ Es classi e oggetti/Program.cs
@@ -3,13 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Classe;
+using 
+
 namespace Es_classi_e_oggetti
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Inserisci il nome: ");
+            string valore1 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Inserisci il prezzo: ");
+            double valore2 = Convert.ToDouble(Console.ReadLine());
+
+            Prodotto prodotto = new Prodotto(valore1, valore2);
+
+            var output = prodotto.Ris();
+            Console.WriteLine("Il nome è: " + output.Item1 + "\nIl prezzo è: " + output.Item2);
         }
     }
 }
---
+++ classeProva/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classeProva
+{
+    public class Class1
+    {
+        public class Prodotto
+        {
+            //attributi
+            private string nome;
+            private double prezzo;
+
+
+            public (string, double) Ris()
+            {
+
+                return (nome, prezzo);
+            }
+
+            // costruttore vuoto
+            public Prodotto() { }
+
+            // costruttore
+            public Prodotto(string nomeVal, double prezzoVal)
+            {
+                nome = nomeVal;
+                prezzo = prezzoVal;
+            }
+        }
+    }
+}
